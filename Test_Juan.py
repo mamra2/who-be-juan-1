50116932f3bcb4cc55aa9a5b4e31e78cb9e5b55f
rfgoo <rfgoo@iscte-iul.pt>
2021-12-08 19:34:33 +0000

    added who be juan trabalho and new gifs to who be juan enri

--- Test_Juan.py
+++ Test_Juan.py
@@ -10,22 +10,24 @@ class MyBot(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        commands = ["who be juan olek", "who be juan enri", "who be juan help", "who be juan", "who be juan trabalho"]
+        commands = ["who be juan olek", "who be juan enri", "who be juan help", "who be juan", "who be juan trabalho", "who be juan trabalha"]
         responses = [f"Olha {message.author.name} sobes ao {random.randint(0,1000)}º andar e saltas.",
-                     "https://w7.pngwing.com/pngs/837/520/png-transparent-a-monkey-s-orientation-chimpanzee-cymbal-banging-monkey-toy-monkey-animals-snout-cymbal-thumbnail.png",
+                     "https://c.tenor.com/lVLSSglhk1cAAAAC/monkey-cymbals.gif", "https://c.tenor.com/7Glf51FDQZQAAAAM/monkey-annoying.gif",
                      "who be juan kanobi",
-                     "Isso dá muito trabalho..."]
+                     "Isso dá muito trabalho...", "@rogue#0001 faz tu!", "https://media1.tenor.com/images/5ad50b6db3dc7ed4ca10dd65d4ea84c2/tenor.gif?itemid=11811769"]
         if message.content == commands[0]:
             await message.channel.send(responses[0])
         elif message.content == commands[1]:
-            await message.channel.send(responses[1])
+            number = random.randint(1, 2)
+            await message.channel.send(responses[number])
         elif message.content == commands[2]:
             response = "\n".join(commands)
             await message.channel.send(response)
         elif message.content == commands[3]:
             await message.channel.send(responses[2])
-        elif message.content == commands[4]:
-            await message.channel.send(responses[3])
+        elif message.content == commands[4] or message.content == commands[5]:
+            number = random.randint(4, 6)
+            await message.channel.send(responses[number])
         if "jura" in message.content:
             await message.channel.send("JUROOOO")
         if "hum" in message.content and not (message.author == self.user):
